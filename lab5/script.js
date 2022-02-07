3cd480a05559a9955b1e1d07a6d4b0f688c5830e
gourav~ <95690791+Gaurav5416@users.noreply.github.com>
2022-02-07 09:22:57 +0530

    commit

---
+++ lab5/script.js
@@ -0,0 +1,12 @@
+console.log("Welcome to Programiz!");
+
+function display() {
+    var name=document.getElementById("name").value
+    var mail=document.getElementById("mail").value
+    var comment=document.getElementById("comment").value
+    alert(name, mail, comment)
+}
+
+document.getElementById('submit').addEventListener("click", ()=>{
+    display()
+})
